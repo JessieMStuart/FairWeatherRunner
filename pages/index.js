14b938cb8b349dfd6eeaa6297be81faa9195c304
Jessie May Stuart <jessiemaystuart@Jessies-MacBook-Air.local>
2020-01-09 22:16:05 +0000

    Added logic to find location and error handling

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,39 @@
 
 
 import React from 'react'
+import axios from 'axios'
 
 class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            weatherData: undefined,
+            error: undefined
+        }
+    }
+
+   async componentDidMount() {
+       try {
+        const result = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=leeds&appid=${process.env.API_KEY}`)
+        this.setState({weatherData: result.data})
+    } catch (e) {
+           this.setState({error: `An error was thrown :( oh no : ${e.message})`})
+       } finally {}
     }
 
     render() {
+        if (this.state.error) {
+            return (
+                <div>
+                    <p>{this.state.error}</p>
+                </div>
+            )
+        }
+
         return (
-            <p>My location is: </p>
+            <div>
+               <p>{this.state.weatherData.name ? `My location is: ${this.state.weatherData.name}` : "Oi I'm loading..."}</p>
+            </div>
         );
     }
 }
